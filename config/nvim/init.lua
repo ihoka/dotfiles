6a3b38abac8d501cecc5b19347469ebcfddf308c
Istvan Hoka <istvan.hoka@gmail.com>
2023-12-05 10:28:04 +0000

    No leap

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,19 +1,9 @@
 require('packer').startup(function(use)
 	use 'echasnovski/mini.ai'
-	use 'ggandor/leap.nvim'
-	use 'ggandor/flit.nvim'
+	use 'echasnovski/mini.surround'
 end)
 
 require('mini.ai').setup()
 
-require('leap').add_default_mappings()
-require('flit').setup {
-  keys = { f = 'f', F = 'F', t = 't', T = 'T' },
-  -- A string like "nv", "nvo", "o", etc.
-  labeled_modes = "v",
-  multiline = true,
-  -- Like `leap`s similar argument (call-specific overrides).
-  -- E.g.: opts = { equivalence_classes = {} }
-  opts = {}
-}
+require('mini.surround').setup()
 
